4c9fd9dbdd9e7bb1b15cfdb64fc31698c4210cec
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2023-10-15 10:39:34 +0200

    Add files via upload
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

---
+++ src/components/request.php
@@ -0,0 +1,168 @@
+<?php
+
+	namespace ClipStack\Component;
+
+	class Request {
+		private static ?Request $instance = null;
+
+		private array $server;
+		private array $post;
+
+		/**
+		 * PRIVATE CONSTRUCTOR FOR SINGLETON PATTERN.
+		 */
+		private function __construct(array $server, array $post) {
+			$this -> server = $server;
+			$this -> post = $post;
+		}
+
+		/**
+		 * SAFETY FOR SINGLETON PATTERN: PREVENT CLONING.
+		 */
+		private function __clone() {}
+
+		/**
+		 * SAFETY FOR SINGLETON PATTERN: PREVENT UNSERIALIZATION.
+		 */
+		private function __wakeup() {}
+
+		/**
+		 * RETRIEVE THE SINGLETON INSTANCE OF THE REQUEST CLASS.
+		 */
+		public static function getInstance(array $server = [], array $post = []): Request {
+			if (self::$instance === null) {
+				// SAFELY CHECK IF SUPERGLOBALS ARE SET AND USE THEM IF NOT OVERRIDDEN BY THE PROVIDED PARAMETERS.
+				$server = !empty($server) ? $server : (isset($_SERVER) ? $_SERVER : []);
+				$post = !empty($post) ? $post : (isset($_POST) ? $_POST : []);
+				self::$instance = new self($server, $post);
+			}
+			return self::$instance;
+		}
+
+		/**
+		 * GET THE CURRENT FULL URL OF THE WEBSITE.
+		 *
+		 * @return string
+		 * 
+		 * @example
+		 * $request = Request::getInstance();
+		 * echo $request->getFullUrl();
+		 */
+		public function getFullUrl(): string {
+			$protocol = (isset($this -> server['HTTPS']) && $this -> server['HTTPS'] === 'on') ? "https" : "http";
+			return $protocol . "://" . ($this -> server['HTTP_HOST'] ?? '') . ($this -> server['REQUEST_URI'] ?? '');
+		}
+
+		/**
+		 * GET THE URI OF THE WEBSITE.
+		 *
+		 * @return string
+		 * 
+		 * @example
+		 * $request = Request::getInstance();
+		 * echo $request->getUri();
+		 */
+		public function getUri(): string {
+			return $this -> server['REQUEST_URI'] ?? '';
+		}
+
+		/**
+		 * GET THE QUERY PARAMETERS OF THE REQUEST.
+		 *
+		 * @return array
+		 * 
+		 * @example
+		 * $request = Request::getInstance();
+		 * print_r($request->getQueryParameters());
+		 */
+		public function getQueryParameters(): array {
+			parse_str($this -> server['QUERY_STRING'] ?? '', $params);
+			return $params;
+		}
+
+		/**
+		 * CHECK IF A POST KEY EXISTS.
+		 *
+		 * @param string $key - THE POST DATA KEY.
+		 * @return bool
+		 *
+		 * @example
+		 * $request = Request::getInstance();
+		 * if ($request->hasPostDataKey('username')) {
+		 *     echo $request->getPostData('username');
+		 * }
+		 */
+		public function hasPostDataKey(string $key): bool {
+			return array_key_exists($key, $this -> post);
+		}
+
+		/**
+		 * GET POST DATA BY KEY, OR RETURN ALL POST DATA IF NO KEY IS PROVIDED.
+		 *
+		 * @param string|null $key - THE KEY FOR THE POST DATA.
+		 * @return array|string|null
+		 *
+		 * @example
+		 * $request = Request::getInstance();
+		 * echo $request->getPostData('username');  // GET SPECIFIC POST DATA BY KEY
+		 * print_r($request->getPostData());        // GET ALL POST DATA
+		 */
+		public function getPostData(string $key = null): array|string|null {
+			if ($key === null) {
+				return $this -> post;
+			}
+			return $this -> post[$key] ?? null;
+		}
+
+		/**
+		 * GET THE USER AGENT STRING.
+		 *
+		 * @return string
+		 * 
+		 * @example
+		 * $request = Request::getInstance();
+		 * echo $request->getUserAgent();
+		 */
+		public function getUserAgent(): string {
+			return $this -> server['HTTP_USER_AGENT'] ?? '';
+		}
+
+		/**
+		 * RETRIEVE A SPECIFIC REQUEST HEADER.
+		 *
+		 * @param string $header - THE HEADER NAME.
+		 * @return string|null
+		 *
+		 * @example
+		 * $request = Request::getInstance();
+		 * echo $request->getHeader('Accept-Language');
+		 */
+		public function getHeader(string $header): ?string {
+			$key = 'HTTP_' . strtoupper(str_replace('-', '_', $header));
+			return $this -> server[$key] ?? null;
+		}
+
+		/**
+		 * RETRIEVE ALL REQUEST HEADERS.
+		 *
+		 * @return array
+		 *
+		 * @example
+		 * $request = Request::getInstance();
+		 * print_r($request->getAllHeaders());
+		 */
+		public function getAllHeaders(): array {
+			if (function_exists('getallheaders')) {
+				return getallheaders();
+			} else {
+				$headers = [];
+				foreach ($this -> server as $key => $value) {
+					if (substr($key, 0, 5) === 'HTTP_') {
+						$header_key = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
+						$headers[$header_key] = $value;
+					}
+				}
+				return $headers;
+			}
+		}
+	}
