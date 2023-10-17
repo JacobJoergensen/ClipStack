1181af9122d4fbe9fd33b0dce857f8147848440a
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2023-10-17 19:56:08 +0200

    Add files via upload
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

---
+++ src/bootstrap.php
@@ -0,0 +1,124 @@
+<?php
+	namespace ClipStack\Bootstrap;
+
+	use ClipStack\Backbone\Version;
+	use ClipStack\Backbone\Config;
+
+	/**
+	 * VALIDATE THAT THE SERVER'S PHP VERSION MEETS ClipStack'S REQUIREMENTS.
+	 */
+	$php_version = phpversion();
+
+	if (version_compare($php_version, '8.0.0', '<')) {
+		throw new \RuntimeException("Your PHP version ($php_version) is below the supported version. Please upgrade to at least 8.0.0!");
+		exit();
+	}
+	
+	if (version_compare($php_version, '8.2.0', '>')) {
+		throw new \RuntimeException("Your PHP version ($php_version) exceeds the maximum supported version. The system supports up to PHP 8.2.0!");
+		exit();
+	}
+	
+	echo "Your PHP version ($php_version) is supported!";
+
+	/**
+	 * SETTING SECURITY HEADERS:
+	 *
+	 * SECURITY HEADERS PLAY A VITAL ROLE IN SAFEGUARDING WEB APPLICATIONS BY INSTRUCTING BROWSERS ON HOW TO
+	 * HANDLE CONTENT. PROPER CONFIGURATION CAN MITIGATE VARIOUS TYPES OF ATTACKS AND SECURITY VULNERABILITIES.
+	 * BY SETTING THESE HEADERS, WE ENHANCE THE APPLICATION'S DEFENSE MECHANISMS AGAINST THREATS LIKE 
+	 * CROSS-SITE SCRIPTING (XSS), CLICKJACKING, MIME TYPE SNIFFING, AND MORE. REGULARLY REVIEWING AND UPDATING
+	 * THESE HEADERS IN ALIGNMENT WITH EVOLVING SECURITY PRACTICES ENSURES THE APPLICATION REMAINS RESILIENT
+	 * AGAINST EMERGING THREATS.
+	 */
+	header("Content-Security-Policy: default-src 'self';");
+	header("X-Frame-Options: SAMEORIGIN");
+	header("X-XSS-Protection: 1; mode=block");
+	header("X-Content-Type-Options: nosniff");
+	header("Referrer-Policy: no-referrer-when-downgrade");
+	header("Strict-Transport-Security: max-age=31536000; includeSubDomains; preload");
+
+	/**
+	 * ENSURE THAT THE WEB SERVER CONFIGURATIONS ARE AS EXPECTED.
+	 */
+	if (function_exists('apache_get_modules') && !in_array('mod_rewrite', apache_get_modules())) {
+		throw new \RuntimeException('mod_rewrite module is not enabled in Apache. Please enable mod_rewrite to continue.');
+		exit();
+	}
+
+	/**
+	 * VERIFY IF THE REQUIRED PHP EXTENSIONS FOR ClipStack ARE LOADED.
+	 */
+	$required_extensions = ['pdo', 'mbstring', 'json', 'curl'];
+
+	foreach ($required_extensions as $extension) {
+		if (!extension_loaded($extension)) {
+			throw new \RuntimeException("The {$extension} extension is not installed or enabled. This extension is required for ClipStack to function properly.");
+			exit();
+		}
+	}
+
+	/**
+	 * ENSURE ClipStack RUNS OVER A SECURE HTTPS CONNECTION.
+	 */
+	if ((!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off') && $_SERVER['SERVER_PORT'] !== '443') {
+		throw new \RuntimeException('ClipStack requires a secure HTTPS connection. Please ensure SSL/TLS is correctly configured.');
+		exit();
+	}
+
+	/**
+	 * LOAD AND VALIDATE THE CONFIGURATION FILE FOR ClipStack.
+	 */
+	$config_array = require __DIR__ . '/backbone/config.php';
+	$config_instance = Config::getInstance($config_array);
+
+	if (!is_array($config_array)) {
+		throw new \RuntimeException('Configuration file did not return a valid array.');
+		exit();
+	}
+
+	/**
+	 * VALIDATE THE ENVIRONMENT SETTING WITHIN THE ClipStack CONFIGURATION.
+	 */
+	$environment = $config_instance -> get('app.env');
+
+	if (!in_array($environment, ['development', 'production'])) {
+		throw new \RuntimeException("The specified environment setting '{$environment}' is not recognized. Valid environments are 'development' or 'production'.");
+		exit();
+	}
+
+	/**
+	 * NOTIFY THE USER IF THERE IS A NEWER VERSION OF ClipStack AVAILABLE.
+	 */
+	$new_version = Version::isNewVersionAvailable();
+
+	if ($new_version !== false) {
+		echo "There's a new version ($new_version) available for ClipStack! Consider updating for the latest features and security fixes.";
+	}
+
+	// REGISTER THE AUTOLOADER.
+	spl_autoload_register(function ($class) {
+		// PROJECT NAMESPACE PREFIX.
+		$prefix = 'ClipStack\\';
+
+		// BASE DIRECTORY FOR THE NAMESPACE PREFIX.
+		$base_dir = __DIR__ . '/';
+
+		// CHECK IF THE CLASS USES THE NAMESPACE PREFIX.
+		$len = strlen($prefix);
+		if (strncmp($prefix, $class, $len) !== 0) {
+			// MOVE TO THE NEXT REGISTERED AUTOLOADER.
+			return;
+		}
+
+		// GET THE RELATIVE CLASS NAME.
+		$relative_class = substr($class, $len);
+
+		// REPLACE THE NAMESPACE PREFIX WITH THE BASE DIRECTORY, REPLACE NAMESPACE SEPARATORS WITH DIRECTORY SEPARATORS.
+		$file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+		// IF THE FILE EXISTS, THEN REQUIRE IT.
+		if (file_exists($file)) {
+			require $file;
+		}
+	});
