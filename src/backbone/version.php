7238c3fd30e07dc9ddff018c96d8da5744f1ba42
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2023-10-17 16:59:44 +0200

    Add config system (#30)
    * Update config.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Add files via upload
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update config.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update config.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update config.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update config.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update config.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Add files via upload
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update version.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update config.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update version.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update config.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update version.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Delete src/components/config.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

---
+++ src/backbone/version.php
@@ -0,0 +1,35 @@
+<?php
+	namespace ClipStack\Backbone;
+
+	class version {
+		public const CURRENT_VERSION = '0.1.0';
+	
+		/**
+		 * CHECK IF THERE'S A NEW VERSION AVAILABLE.
+		 * 
+		 * @return string|bool - RETURNS THE NEW VERSION IF AVAILABLE, OR FALSE IF THE CURRENT VERSION IS THE LATEST.
+		 * 
+		 * @example
+		 * $newVersion = FrameworkVersion::isNewVersionAvailable();
+		 * if ($newVersion !== false) {
+		 *     echo "New version available: $newVersion";
+		 * } else {
+		 *     echo "You are using the latest version.";
+		 * }
+		 */
+		public static function isNewVersionAvailable(): string|bool {
+			$latest_version = file_get_contents('TODO - ADD URL TO THE VERSION FILE HERE ON GITHUB');
+
+			// CHECK IF $latest_version IS A VALID STRING.
+			if ($latest_version === false || !is_string($latest_version)) {
+				// NO NEW VERSION.
+				return false;
+			}
+
+			if (version_compare(self::CURRENT_VERSION, trim($latest_version), '<')) {
+				return $latest_version;
+			}
+
+			return false;
+		}
+	}
