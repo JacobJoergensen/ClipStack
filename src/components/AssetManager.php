b24548c2207b2e994aef41300df13b7e8de99ba6
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2023-10-24 10:22:00 +0200

    Add files via upload
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

---
+++ src/components/AssetManager.php
@@ -0,0 +1,189 @@
+<?php
+	namespace ClipStack\Component;
+
+	class StylesheetFile {
+		/**
+		 * STYLESHEETFILE CONSTRUCTOR.
+		 *
+		 * @param string $path - PATH TO THE STYLESHEET FILE.
+		 * @param string|null $integrity - OPTIONAL INTEGRITY HASH FOR THE STYLESHEET.
+		 */
+		public function __construct(
+			private string $path,
+			private ?string $integrity = null
+		) {}
+
+		/**
+		 * GET THE PATH OF THE STYLESHEET.
+		 *
+		 * @return string
+		 */
+		public function getPath(): string {
+			return $this -> path;
+		}
+
+		/**
+		 * GET THE INTEGRITY HASH OF THE STYLESHEET.
+		 *
+		 * @return string|null
+		 */
+		public function getIntegrity(): ?string {
+			return $this -> integrity;
+		}
+	}
+
+	class ScriptFile {
+		/**
+		 * SCRIPTFILE CONSTRUCTOR.
+		 *
+		 * @param string $path - PATH TO THE SCRIPT FILE.
+		 * @param string|null $integrity - OPTIONAL INTEGRITY HASH FOR THE SCRIPT.
+		 * @param bool $async - INDICATES IF SCRIPT SHOULD BE LOADED ASYNCHRONOUSLY.
+		 * @param bool $defer - INDICATES IF SCRIPT EXECUTION SHOULD BE DEFERRED.
+		 */
+		public function __construct(
+			private string $path,
+			private ?string $integrity = null,
+			private bool $async = false,
+			private bool $defer = false
+		) {}
+
+		/**
+		 * GET THE PATH OF THE SCRIPT.
+		 *
+		 * @return string
+		 */
+		public function getPath(): string {
+			return $this -> path;
+		}
+
+		/**
+		 * GET THE INTEGRITY HASH OF THE SCRIPT.
+		 *
+		 * @return string|null
+		 */
+		public function getIntegrity(): ?string {
+			return $this -> integrity;
+		}
+
+
+		/**
+		 * CHECK IF THE SCRIPT SHOULD BE LOADED ASYNCHRONOUSLY.
+		 *
+		 * @return bool
+		 */
+		public function isAsync(): bool {
+			return $this -> async;
+		}
+
+		/**
+		 * CHECK IF THE SCRIPT EXECUTION SHOULD BE DEFERRED.
+		 *
+		 * @return bool
+		 */
+		public function isDefer(): bool {
+			return $this -> defer;
+		}
+	}
+
+	class CSS {
+		/**
+		 * @var StylesheetFile[]
+		 */
+		private array $files = [];
+
+		/**
+		 * IMPORT A NEW STYLESHEET FILE.
+		 *
+		 * @param string $path - PATH TO THE STYLESHEET FILE.
+		 * @param string|null $integrity - OPTIONAL INTEGRITY HASH FOR THE STYLESHEET.
+		 *
+		 * @example
+		 * $css->import('/path/to/style.css');
+		 */
+		public function import(string $path, ?string $integrity = null): void {
+			$this -> files[] = new StylesheetFile($path, $integrity);
+		}
+
+		/**
+		 * RENDER THE IMPORTED STYLESHEET LINKS.
+		 *
+		 * @return string
+		 *
+		 * @example
+		 * echo $css->render();
+		 */
+		public function render(): string {
+			$html = '';
+
+			foreach($this -> files as $file) {
+				$html .= '    <link rel="stylesheet" href="' . $file -> getPath() . '"';
+
+				if (!empty($file -> getIntegrity())) {
+					$html .= ' integrity="' . $file -> getIntegrity() . '"';
+					$html .= ' crossorigin="anonymous"';
+					$html .= ' referrerpolicy="no-referrer"';
+				}
+
+				$html .= '>' . PHP_EOL;
+			}
+
+			return $html;
+		}
+	}
+
+	class JS {
+		/**
+		 * @var ScriptFile[]
+		 */
+		private array $files = [];
+
+		/**
+		 * IMPORT A NEW SCRIPT FILE.
+		 *
+		 * @param string $path - PATH TO THE SCRIPT FILE.
+		 * @param string|null $integrity - OPTIONAL INTEGRITY HASH FOR THE SCRIPT.
+		 * @param bool $async - INDICATES IF SCRIPT SHOULD BE LOADED ASYNCHRONOUSLY.
+		 * @param bool $defer - INDICATES IF SCRIPT EXECUTION SHOULD BE DEFERRED.
+		 *
+		 * @example
+		 * $js->import('/path/to/script.js', null, true, false);
+		 */
+		public function import(string $path, ?string $integrity = null, bool $async = false, bool $defer = false): void {
+			$this -> files[] = new ScriptFile($path, $integrity, $async, $defer);
+		}
+
+		/**
+		 * RENDER THE IMPORTED SCRIPT TAGS.
+		 *
+		 * @return string
+		 *
+		 * @example
+		 * echo $js->render();
+		 */
+		public function render(): string {
+			$html = '';
+
+			foreach($this->files as $file) {
+				$html .= '    <script src="' . $file -> getPath() . '"';
+
+				if (!empty($file -> getIntegrity())) {
+					$html .= ' integrity="' . $file -> getIntegrity() . '"';
+					$html .= ' crossorigin="anonymous"';
+					$html .= ' referrerpolicy="no-referrer"';
+				}
+
+				if ($file -> isAsync()) {
+					$html .= ' async';
+				}
+
+				if ($file -> isDefer()) {
+					$html .= ' defer';
+				}
+
+				$html .= '></script>' . PHP_EOL;
+			}
+
+			return $html;
+		}
+	}
