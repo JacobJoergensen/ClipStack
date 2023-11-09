d27654a0a287e4951883a46b0a21b765efcc09ee
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2023-11-09 19:10:20 +0100

    Update Validate.php
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

--- src/components/Validate.php
+++ src/components/Validate.php
@@ -2,12 +2,12 @@
 	namespace ClipStack\Component;
 
 	class Validate {
-		private DateTimeUtility $dateTimeUtility;
-		private ErrorHandler $errorHandler;
+		private DateTimeUtility $date_time_utility;
+		private ErrorHandler $error_handler;
 
-		public function __construct(DateTimeUtility $dateTimeUtility, ErrorHandler $errorHandler) {
-			$this -> dateTimeUtility = $dateTimeUtility;
-			$this -> errorHandler = $errorHandler;
+		public function __construct(DateTimeUtility $date_time_utility, ErrorHandler $error_handler) {
+			$this -> date_time_utility = $date_time_utility;
+			$this -> error_handler = $error_handler;
 		}
 
 		/**
@@ -15,10 +15,10 @@
 		 *
 		 * @param string $data
 		 * @return string
-		 * 
+		 *
 		 * @example
 		 * $validate = new Validate(new DateTimeUtility(new Config()));
-		 * $result = $validate->sanitizeString('<b>bold</b>');  // &lt;b&gt;bold&lt;/b&gt;
+		 * $result = $validate -> sanitizeString('<b>bold</b>');  // &lt;b&gt;bold&lt;/b&gt;
 		 */
 		public function sanitizeString(string $data): string {
 			return htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
@@ -29,10 +29,10 @@
 		 *
 		 * @param array<mixed> $data - AN ARRAY CONTAINING STRINGS OR OTHER ARRAYS TO SANITIZE.
 		 * @return array<mixed> - THE SANITIZED ARRAY.
-		 * 
+		 *
 		 * @example
 		 * $validate = new Validate(new DateTimeUtility(new Config()));
-		 * $result = $validate->isEmail('test@example.com');  // true
+		 * $result = $validate -> isEmail('test@example.com');  // true
 		 */
 		public function sanitizeArray(array $data): array {
 			foreach ($data as $key => $value) {
@@ -51,20 +51,20 @@
 		 *
 		 * @param string $string
 		 * @return bool
-		 * 
+		 *
 		 * @example
 		 * $validate = new Validate(new DateTimeUtility(new Config()), new ErrorHandler());
-		 * $result = $validate->isEmpty('    ');  // true
-		 * $result = $validate->isEmpty('test');  // false
+		 * $result = $validate -> isEmpty('    ');  // true
+		 * $result = $validate -> isEmpty('test');  // false
 		 */
 		public function isEmpty(string $string): bool {
-			$isEmpty = trim($string) === '';
+			$is_empty = trim($string) === '';
 
-			if ($isEmpty) {
-				$this -> errorHandler -> setError('string', 'The string is empty or contains only whitespace.');
+			if ($is_empty) {
+				$this -> error_handler -> setError('string', 'The string is empty or contains only whitespace.');
 			}
 
-			return $isEmpty;
+			return $is_empty;
 		}
 
 		/**
@@ -72,21 +72,21 @@
 		 *
 		 * @param mixed $data
 		 * @return bool
-		 * 
+		 *
 		 * @example
 		 * $validate = new Validate();
-		 * $result = $validate->isNumeric('12345');  // true
-		 * $result = $validate->isNumeric('12.345'); // true
-		 * $result = $validate->isNumeric('abc');    // false
+		 * $result = $validate -> isNumeric('12345');  // true
+		 * $result = $validate -> isNumeric('12.345');  // true
+		 * $result = $validate -> isNumeric('abc');  // false
 		 */
 		public function isNumeric(mixed $data): bool {
-			$isNumeric = is_numeric($data);
+			$is_numeric = is_numeric($data);
 
-			if (!$isNumeric) {
-				$this -> errorHandler -> setError('data', 'The input is not numeric.');
+			if (!$is_numeric) {
+				$this -> error_handler -> setError('data', 'The input is not numeric.');
 			}
 
-			return $isNumeric;
+			return $is_numeric;
 		}
 
 		/**
@@ -94,15 +94,15 @@
 		 *
 		 * @param string $data
 		 * @return bool
-		 * 
+		 *
 		 * @example
 		 * $validate = new Validate();
-		 * $result = $validate->isAlpha('abcABC');  // true
-		 * $result = $validate->isAlpha('abc123');  // false
+		 * $result = $validate -> isAlpha('abcABC');  // true
+		 * $result = $validate -> isAlpha('abc123');  // false
 		 */
 		public function isAlpha(string $data): bool {
 			if (!ctype_alpha($data)) {
-				$this -> errorHandler -> setError('alpha', 'Input contains non-alphabetic characters.');
+				$this -> error_handler -> setError('alpha', 'Input contains non-alphabetic characters.');
 
 				return false;
 			}
@@ -115,15 +115,15 @@
 		 *
 		 * @param string $data
 		 * @return bool
-		 * 
+		 *
 		 * @example
 		 * $validate = new Validate();
-		 * $result = $validate->isAlphaNumeric('abc123');  // true
-		 * $result = $validate->isAlphaNumeric('abc!@#');  // false
+		 * $result = $validate -> isAlphaNumeric('abc123');  // true
+		 * $result = $validate -> isAlphaNumeric('abc!@#');  // false
 		 */
 		public function isAlphaNumeric(string $data): bool {
 			if (!ctype_alnum($data)) {
-				$this -> errorHandler -> setError('alphaNumeric', 'Input contains characters other than alphabets and numbers.');
+				$this -> error_handler -> setError('alphaNumeric', 'Input contains characters other than alphabets and numbers.');
 
 				return false;
 			}
@@ -135,28 +135,28 @@
 		 * VALIDATES IF THE INPUT STRING IS A VALID DATE.
 		 *
 		 * @param string $date
-		 * @param string $format Default format is 'Y-m-d' (e.g., "2023-10-15"). For other formats, supply as the second argument.
+		 * @param string $format - DEFAULT FORMAT IS 'Y-m-d' (e.g., "2023-10-15"). FOR OTHER FORMATS, SUPPLY AS THE SECOND ARGUMENT.
 		 * @return bool
-		 * 
+		 *
 		 * @example
 		 * $validate = new Validate(new DateTimeUtility(new Config()));
-		 * $result = $validate->isDate('2023-10-15');               // true
-		 * $result = $validate->isDate('15-10-2023', 'd-m-Y');      // true
-		 * $result = $validate->isDate('October 15, 2023', 'F j, Y'); // true
+		 * $result = $validate -> isDate('2023-10-15');  // true
+		 * $result = $validate -> isDate('15-10-2023', 'd-m-Y');  // true
+		 * $result = $validate -> isDate('October 15, 2023', 'F j, Y');  // true
 		 */
 		public function isDate(string $date, string $format = 'Y-m-d'): bool {
 			try {
-				$date_time = $this -> dateTimeUtility -> formatDateTime($date, $format);
+				$date_time = $this -> date_time_utility -> formatDateTime($date, $format);
 
 				if ($date_time !== $date) {
-					$this -> errorHandler -> setError('date', "Date format mismatch. Expected format: $format");
+					$this -> error_handler -> setError('date', "Date format mismatch. Expected format: $format");
 
 					return false;
 				}
 
 				return true;
 			} catch (\Exception $e) {
-				$this -> errorHandler -> setError('date', 'Invalid date provided.');
+				$this -> error_handler -> setError('date', 'Invalid date provided.');
 
 				return false;
 			}
@@ -164,7 +164,7 @@
 
 		/**
 		 * CONSOLIDATED EMAIL VALIDATION.
-		 * 
+		 *
 		 * @param string $email - THE EMAIL TO BE VALIDATED.
 		 * @return string|false - THE VALIDATED EMAIL OR FALSE IF IT'S INVALID.
 		 */
@@ -177,17 +177,17 @@
 		 *
 		 * @param string $email
 		 * @return bool
-		 * 
+		 *
 		 * @example
 		 * $validate = new Validate(new DateTimeUtility(new Config()), new ErrorHandler());
-		 * $result = $validate->isEmail('test@example.com');  // true
-		 * $result = $validate->isEmail('test@example');      // false
+		 * $result = $validate -> isEmail('test@example.com');  // true
+		 * $result = $validate -> isEmail('test@example');      // false
 		 */
 		public function isEmail(string $email): bool {
 			$is_valid = $this -> performEmailValidation($email) !== false;
 
 			if (!$is_valid) {
-				$this -> errorHandler -> setError('email', 'Invalid email address provided.');
+				$this -> error_handler -> setError('email', 'Invalid email address provided.');
 			}
 
 			return $is_valid;
@@ -198,17 +198,17 @@
 		 *
 		 * @param string $email
 		 * @return string|null
-		 * 
+		 *
 		 * @example
 		 * $validate = new Validate(new DateTimeUtility(new Config()), new ErrorHandler());
-		 * $filteredEmail = $validate->filterEmail('test@example.com');  // 'test@example.com'
-		 * $result = $validate->filterEmail('test@example');            // false
+		 * $filteredEmail = $validate -> filterEmail('test@example.com');  // 'test@example.com'
+		 * $result = $validate -> filterEmail('test@example');  // false
 		 */
 		public function filterEmail(string $email): ?string {
 			$validated_email = $this -> performEmailValidation($email);
 
 			if ($validated_email === false) {
-				$this -> errorHandler -> setError('email', 'Invalid email address provided.');
+				$this -> error_handler -> setError('email', 'Invalid email address provided.');
 
 				return null;
 			}
@@ -221,17 +221,17 @@
 		 *
 		 * @param string $phone
 		 * @return bool
-		 * 
+		 *
 		 * @example
 		 * $validate = new Validate(new DateTimeUtility(new Config()));
-		 * $result = $validate->isPhoneNumber('+1 555-555-5555');  // true
+		 * $result = $validate -> isPhoneNumber('+1 555-555-5555');  // true
 		 */
 		public function isPhoneNumber(string $phone): bool {
 			$pattern = "/^(\+?\d{1,4})?\s?-?\d{10,11}$/";
 			$is_valid = preg_match($pattern, $phone) === 1;
 
 			if (!$is_valid) {
-				$this -> errorHandler -> setError('phone', 'Invalid phone number format.');
+				$this -> error_handler -> setError('phone', 'Invalid phone number format.');
 			}
 
 			return $is_valid;
@@ -252,16 +252,16 @@
 		 *
 		 * @param string $url
 		 * @return bool
-		 * 
+		 *
 		 * @example
 		 * $validate = new Validate(new DateTimeUtility(new Config()));
-		 * $result = $validate->isURL('https://www.example.com');  // true
+		 * $result = $validate -> isURL('https://www.example.com');  // true
 		 */
 		public function isURL(string $url): bool {
 			$is_valid = $this -> performURLValidation($url) !== false;
 
 			if (!$is_valid) {
-				$this -> errorHandler -> setError('url', 'Invalid URL format.');
+				$this -> error_handler -> setError('url', 'Invalid URL format.');
 			}
 
 			return $is_valid;
@@ -272,24 +272,23 @@
 		 *
 		 * @param string $url
 		 * @return string|null
-		 * 
+		 *
 		 * @example
 		 * $validate = new Validate(new DateTimeUtility(new Config()), new ErrorHandler());
-		 * $filteredURL = $validate->filterURL('https://www.example.com');  // 'https://www.example.com'
-		 * $result = $validate->filterURL('not a url');                      // false
+		 * $filteredURL = $validate -> filterURL('https://www.example.com');  // 'https://www.example.com'
+		 * $result = $validate -> filterURL('not a url');  // false
 		 */
 		public function filterURL(string $url): ?string {
 			$validated_url = $this -> performURLValidation($url);
 
 			if ($validated_url === false) {
-				$this -> errorHandler -> setError('url', 'Invalid URL format.');
+				$this -> error_handler -> setError('url', 'Invalid URL format.');
 
 				return null;
 			}
-	
+
 			return $validated_url;
 		}
-
 
 		/**
 		 * CHECK IF THE STRING'S LENGTH IS WITHIN A SPECIFIED RANGE.
@@ -298,17 +297,17 @@
 		 * @param int $min
 		 * @param int $max
 		 * @return bool
-		 * 
+		 *
 		 * @example
 		 * $validate = new Validate(new DateTimeUtility(new Config()));
-		 * $result = $validate->stringLength('test', 2, 10);  // true
+		 * $result = $validate -> stringLength('test', 2, 10);  // true
 		 */
 		public function stringLength(string $data, int $min = 0, int $max = PHP_INT_MAX): bool {
 			$length = mb_strlen($data);
 			$is_valid = $length >= $min && $length <= $max;
 
 			if (!$is_valid) {
-				$this -> errorHandler -> setError('data', "The string's length is out of the specified range.");
+				$this -> error_handler -> setError('data', "The string's length is out of the specified range.");
 			}
 
 			return $is_valid;
@@ -323,7 +322,8 @@
 		 */
 		public function isValidSqlName(string $name): bool {
 			if (!preg_match('/^[a-zA-Z0-9_]+$/', $name)) {
-				$this -> errorHandler -> setError('sqlName', 'Invalid SQL name provided.');
+				$this -> error_handler -> setError('sqlName', 'Invalid SQL name provided.');
+
 				return false;
 			}
 
@@ -345,9 +345,11 @@
 
 				// SPLIT FIELD DEFINITION INTO NAME AND TYPE/CONSTRAINT.
 				$field_parts = explode(' ', $field, 2);
+
 				if (!$this -> isValidSqlName($field_parts[0])) {
 					return false;
 				}
+
 				// TODO: Further validations for type/constraints can be added here.
 			}
 
