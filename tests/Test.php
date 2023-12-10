b47855647fee08ae2de3a9129c6730b6107a21df
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2023-12-10 20:24:07 +0100

    Update Test.php
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

--- tests/Test.php
+++ tests/Test.php
@@ -1,9 +1,10 @@
 <?php
+	namespace Tests;
 
-use PHPUnit\Framework\TestCase;
+	use PHPUnit\Framework\TestCase;
 
-class Test extends TestCase {
-	public function testAddition(): void {
-		$this -> assertTrue(true);
+	class Test extends TestCase {
+		public function testAddition(): void {
+			$this -> assertTrue(true);
+		}
 	}
-}
