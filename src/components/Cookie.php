6d741cfc39450d55fd9925a816bcf5e823208603
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2023-12-15 21:30:18 +0100

    Update Cookie.php
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

--- src/components/Cookie.php
+++ src/components/Cookie.php
@@ -4,20 +4,44 @@
 	use ClipStack\Component\Backbone\Config;
 
 	use AllowDynamicProperties;
-	use DateTime;
 	use InvalidArgumentException;
 	use JsonException;
 	use RuntimeException;
 
 	#[AllowDynamicProperties] class Cookie {
+		/**
+		 * @var Config
+		 */
 		private Config $config;
-		
-		private string $prefix;
-		private string $same_site;
-		private bool $secure;
-		private bool $http_only;
-		
 
+		/**
+		 * @var string|mixed
+		 */
+		private string $prefix;
+
+		/**
+		 * @var array
+		 */
+		private array $default_attributes = [
+			'expires' => 0,
+			'path' => '/',
+			'domain' => '',
+			'secure' => false,
+			'httponly' => false,
+			'samesite' => 'Lax'
+		];
+
+		/**
+		 * @var array
+		 */
+		private array $cookie_attributes = [];
+
+		/**
+		 * @param Config $config - CREATE AN INSTANCE OF THE COOKIE CLASS.
+		 * IT REQUIRES A CONFIG CLASS OBJECT TO GET THE SESSION SETTINGS STORED IN IT.
+		 *
+		 * @throws RuntimeException - IF THE SESSION CONFIGURATIONS AREN'T AN ARRAY OR IF THE COOKIE_PREFIX IS INVALID.
+		 */
 		public function __construct(Config $config) {
 			$this -> config = $config;
 
@@ -28,93 +52,138 @@
 			}
 
 			$cookie_prefix = $configurations['cookie_prefix'] ?? '';
-			$same_site = $configurations['same_site'] ?? '';
 			$cookie_secure = $configurations['cookie_secure'] ?? false;
 			$cookie_http_only = $configurations['cookie_http_only'] ?? true;
 
-			if (empty($cookie_prefix) || empty($same_site) || !is_bool($cookie_secure) || !is_bool($cookie_http_only)) {
+			$this -> default_attributes['secure'] = $cookie_secure;
+			$this -> default_attributes['httponly'] = $cookie_http_only;
+
+
+			if (empty($cookie_prefix)) {
 				throw new RuntimeException('Invalid cookie configuration.');
 			}
 
 			$this -> prefix = $cookie_prefix;
-			$this -> same_site = $same_site;
-			$this -> secure = $cookie_secure;
-			$this -> http_only = $cookie_http_only;
+		}
+
+		/**
+		 * SET DEFAULT ATTRIBUTES FOR ALL COOKIES SET BY AN INSTANCE OF COOKIE CLASS.
+		 *
+		 * @param array $attributes - DEFAULT COOKIE ATTRIBUTES.
+		 *
+		 * @return void - THIS METHOD DOES NOT RETURN A VALUE.
+		 */
+		public function setDefaultAttributes(array $attributes): void {
+			$this -> default_attributes = array_merge($this -> default_attributes, $attributes);
+		}
+
+		/**
+		 * GET ALL ATTRIBUTES OF A SPECIFIC COOKIE.
+		 *
+		 * @param string $name - THE NAME OF THE COOKIE.
+		 *
+		 * @return array|null - RETURNS ARRAY OF COOKIE ATTRIBUTES OR NULL IF COOKIE NAME ISN'T REGISTERED.
+		 */
+		public function getAttributes(string $name): ?array {
+			$name_with_prefix = $this -> prefix . $name;
+
+			if (!isset($_COOKIE[$name_with_prefix])) {
+				return null;
+			}
+
+			return $this -> cookie_attributes[$name] ?? null;
 		}
 
 		/**
 		 * SET A COOKIE.
 		 *
-		 * @param string $name
-		 * @param mixed $value
-		 * @param DateTime|int $expires
-		 * @param string $path
-		 * @param string $domain
-		 * @param array<string, mixed> $additional_attributes
+		 * @param string $name - THE NAME OF THE COOKIE.
+		 * @param mixed $value - THE VALUE OF THE COOKIE.
+		 * @param array<string, mixed> $attributes - THE ATTRIBUTES FOR THE COOKIE.
 		 *
-		 * @throws RuntimeException
-		 * @throws InvalidArgumentException
+		 * @throws RuntimeException - IF THE COOKIE CONFIGURATION IS NOT VALID.
+		 * @throws InvalidArgumentException - IF THE NAME OF THE COOKIE IS EMPTY OR INVALID, OR IF THE COOKIE VALUE IS TOO LONG.
+		 * @throws JsonException - IF JSON ENCODING OF THE COOKIE VALUE FAILS.
+		 *
+		 * @example
+		 * $cookie -> set('example_cookie', 'example_value');
+		 *
+		 * // ATTRIBUTES EXAMPLE: SET COOKIE ATTRIBUTES.
+		 * $attributes = [
+		 * 'expires' => time() + 3600, // EXPIRES IN 1 HOUR.
+		 * 'path' => '/path',
+		 * 'domain' => 'example.com',
+		 * 'secure' => true,
+		 * 'httponly' => true,
+		 * 'samesite' => 'lax'
+		 * ];
+		 *
+		 * $cookie -> set('example_cookie_with_attributes', ['key' => 'value'], $attributes);
 		 */
-		public function set(
-			string $name,
-			mixed $value,
-			DateTime|int $expires = 0,
-			string $path = '/',
-			string $domain = '',
-			array $additional_attributes = []
-		): void {
+		public function set(string $name, mixed $value, array $attributes = []): void {
+			if (!$name) {
+				throw new InvalidArgumentException('Cookie name cannot be empty.');
+			}
+
+			if (preg_match('/[=,; \t\r\n\013\014]/', $name)) {
+				throw new InvalidArgumentException('Cookie name cannot contain invalid characters: =,; \t\r\n\013\014');
+			}
+
+			if (!is_string($value) && !is_array($value)) {
+				throw new InvalidArgumentException('Invalid value for cookie. Value should be a string or an array.');
+			}
+
+			if (is_string($value) && strlen($value) > 4096) {
+				throw new InvalidArgumentException('Cookie value cannot exceed 4096 characters.');
+			}
+
+			$final_attributes = array_merge($this -> default_attributes, $this -> cookie_attributes[$name]??[], $attributes);
+
+			$this -> cookie_attributes[$name] = $final_attributes;
 			$name = $this -> prefix . $name;
 
-			if ($expires instanceof DateTime) {
-				$expires = $expires -> getTimestamp();
-			} elseif (!is_numeric($expires)) {
-				throw new InvalidArgumentException('Invalid expiration time format.');
+			if (is_array($value)) {
+				$value = json_encode($value, JSON_THROW_ON_ERROR);
 			}
 
-			/** @var array{expires?: int, path?: string, domain?: string, secure?: bool, httponly?: bool, samesite?: 'Lax'|'lax'|'None'|'none'|'Strict'|'strict'} */
-			$cookie_attributes = array_merge([
-				'expires' => (int)$expires,
-				'path' => $path,
-				'domain' => $domain,
-				'secure' => $this -> secure,
-				'httponly' => $this -> http_only,
-				'samesite' => $this -> same_site
-			], $additional_attributes);
-
-			try {
-				$encoded_value = is_array($value) ? json_encode($value, JSON_THROW_ON_ERROR) : $value;
-
-				if (!is_string($encoded_value)) {
-					throw new InvalidArgumentException('Invalid value type. Expected a string.');
-				}
-			} catch (JsonException $exception) {
-				// Handle the JSON encoding error, e.g., log it or throw a more specific exception.
-				throw new RuntimeException('Error encoding value to JSON: ' . $exception -> getMessage(), $exception -> getCode(), $exception);
-			}
-
-			setcookie($name, $encoded_value, $cookie_attributes);
+			setcookie($name, $value, $final_attributes);
 		}
 
 		/**
 		 * GET THE VALUE OF A COOKIE.
 		 *
-		 * @param string $name
-		 * @param string $default
+		 * @param string $name - THE NAME OF THE COOKIE.
 		 *
-		 * @return string
+		 * @return mixed - THE VALUE OF THE COOKIE WHICH CAN BE OF ANY TYPE OR NULL IF COOKIE DOES NOT EXIST.
+		 *
+		 * @throws JsonException - IF JSON DECODING FAILED.
 		 */
-		public function get(string $name, string $default = ''): string {
+		public function get(string $name): mixed {
 			$name = $this -> prefix . $name;
 
-			return $_COOKIE[$name] ?? $default;
+			if (!isset($_COOKIE[$name])) {
+				return null;
+			}
+
+			$value = $_COOKIE[$name];
+
+			$decoded = json_decode($value, true, 512, JSON_THROW_ON_ERROR);
+
+			if (json_last_error() === JSON_ERROR_NONE) {
+				return $decoded;
+			}
+
+			return $value;
 		}
 
 		/**
 		 * DELETE A COOKIE.
 		 *
-		 * @param string $name
-		 * @param string $path
-		 * @param string $domain
+		 * @param string $name - THE NAME OF THE COOKIE.
+		 * @param string $path - (OPTIONAL) THE PATH WHERE THE COOKIE EXISTS.
+		 * @param string $domain - (OPTIONAL) THE DOMAIN FROM WHERE THE COOKIE EXISTS.
+		 *
+		 * @return void - THIS METHOD DOES NOT RETURN A VALUE.
 		 */
 		public function delete(string $name, string $path = '/', string $domain = ''): void {
 			$name = $this -> prefix . $name;
@@ -127,9 +196,9 @@
 		/**
 		 * CHECK IF A COOKIE EXISTS.
 		 *
-		 * @param string $name
+		 * @param string $name - THE NAME OF THE COOKIE.
 		 *
-		 * @return bool
+		 * @return bool - TRUE IF COOKIE EXISTS, FALSE OTHERWISE.
 		 */
 		public function exists(string $name): bool {
 			$name = $this -> prefix . $name;
@@ -138,11 +207,11 @@
 		}
 
 		/**
-		 * GET ALL COOKIES.
+		 * GET ALL COOKIES SET BY THIS CLASS INSTANCE.
 		 *
-		 * @return array<string, string>
+		 * @return array<string, string> - ALL COOKIES IN ASSOCIATIVE ARRAY WHERE KEY IS COOKIE NAME AND VALUE IS COOKIE VALUE.
 		 *
-		 * @throws RuntimeException
+		 * @throws RuntimeException - IF ANY OPERATION ON COOKIES FAILED.
 		 */
 		public function getAll(): array {
 			$cookies = [];
@@ -157,13 +226,14 @@
 		}
 
 		/**
-		 * FILTER COOKIES BASED ON CRITERIA.
+		 * FILTER COOKIES BASED ON USER-PROVIDED CRITERIA.
 		 *
-		 * @param callable $filter
+		 * @param callable $filter - CALLABLE TO USE FOR FILTERING COOKIES.
+		 * IT MUST TAKE TWO PARAMETERS - COOKIE NAME AND VALUE - AND RETURN BOOLEAN.
 		 *
-		 * @return array<string, string>
+		 * @return array<string, string> - FILTERED COOKIES IN ASSOCIATIVE ARRAY WHERE KEY IS COOKIE NAME AND VALUE IS COOKIE VALUE.
 		 *
-		 * @throws RuntimeException
+		 * @throws RuntimeException - IF ANY OPERATION ON COOKIES FAILED.
 		 */
 		public function filter(callable $filter): array {
 			$filtered_cookies = [];
