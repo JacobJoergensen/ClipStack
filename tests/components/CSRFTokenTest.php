307480708df24d300f2dbf2de207d880ef931407
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2023-12-16 08:42:23 +0100

    Update CSRFTokenTest.php
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

--- tests/components/CSRFTokenTest.php
+++ tests/components/CSRFTokenTest.php
@@ -2,72 +2,55 @@
 	namespace Tests\Component;
 
 	use PHPUnit\Framework\TestCase;
+
+	use ClipStack\Component\Backbone\Config;
 	use ClipStack\Component\CSRFToken;
 	use ClipStack\Component\Session;
+
 	use Random\RandomException;
 
 	class CSRFTokenTest extends TestCase {
+		private Config $config;
+
+		private CSRFToken $csrf;
+
+		private Session $session;
+
+		public function setUp(): void {
+			$this -> config = $this -> createMock(Config::class);
+			$this -> session = $this -> createMock(Session::class);
+
+			$csrf_configs = array(
+				'key' => 'testKey',
+				'lifetime' => '300'
+			);
+
+			$this -> config
+				-> method('get')
+				-> willReturn($csrf_configs);
+		}
+
+		public function testConstructor(): void {
+			$this -> csrf = new CSRFToken($this -> config, $this -> session);
+			$this -> assertInstanceOf(CSRFToken::class, $this -> csrf);
+		}
+
 		/**
 		 * @throws RandomException
 		 */
 		public function testGenerateCSRFToken(): void {
-			$session = $this -> createMock(Session::class);
-			$session -> expects($this -> once())
-				-> method('set')
-				-> with(CSRFToken::CSRF_TOKEN_KEY, $this -> isType('array'));
-
-			$csrf_token = new CSRFToken($session);
-
-			$token = $csrf_token -> generateCSRFToken();
-
+			$this -> csrf = new CSRFToken($this -> config, $this -> session);
+			$token = $this -> csrf -> generateCSRFToken();
 			$this -> assertIsString($token);
-			$this -> assertNotEmpty($token);
 		}
 
-		public function testValidateCSRFTokenSuccess(): void {
-			$session = $this -> createMock(Session::class);
-			$session -> expects($this -> once())
-				-> method('get')
-				-> with(CSRFToken::CSRF_TOKEN_KEY)
-				-> willReturn(['token' => 'test_token', 'expires' => time() + 900]);
-			$session -> expects($this -> once())
-				-> method('remove')
-				-> with(CSRFToken::CSRF_TOKEN_KEY);
-
-			$csrf_token = new CSRFToken($session);
-
-			$result = $csrf_token -> validateCSRFToken('test_token');
-
+		/**
+		 * @throws RandomException
+		 */
+		public function testValidateCSRFToken(): void {
+			$this -> csrf = new CSRFToken($this -> config, $this -> session);
+			$token = $this -> csrf -> generateCSRFToken();
+			$result = $this -> csrf -> validateCSRFToken($token, 1, true);
 			$this -> assertTrue($result);
-		}
-
-		public function testValidateCSRFTokenFailure(): void {
-			$session = $this -> createMock(Session::class);
-			$session -> expects($this -> once())
-				-> method('get')
-				-> with(CSRFToken::CSRF_TOKEN_KEY)
-				-> willReturn(['token' => 'test_token', 'expires' => time() - 900]);
-			$session -> expects($this -> never())
-				-> method('remove');
-
-			$csrf_token = new CSRFToken($session);
-
-			$result = $csrf_token -> validateCSRFToken('test_token');
-
-			$this -> assertFalse($result);
-		}
-
-		public function testClearExpiredTokens(): void {
-			$session = $this -> createMock(Session::class);
-			$session -> expects($this -> once())
-				-> method('get')
-				-> with(CSRFToken::CSRF_TOKEN_KEY)
-				-> willReturn(['expires' => time() - 900]);
-			$session -> expects($this -> once())
-				-> method('remove')
-				-> with(CSRFToken::CSRF_TOKEN_KEY);
-
-			$csrf_token = new CSRFToken($session);
-			$csrf_token -> clearExpiredTokens();
 		}
 	}
