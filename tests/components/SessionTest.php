fa8d8ad39cd34a1277f2d9c8192198356dc2a666
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2024-01-04 19:50:54 +0100

    Update SessionTest.php
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

--- tests/components/SessionTest.php
+++ tests/components/SessionTest.php
@@ -4,68 +4,10 @@
 	use PHPUnit\Framework\TestCase;
 
 	use ClipStack\Component\Session;
-	use ClipStack\Component\Request;
-	use ClipStack\Component\Backbone\Config;
 
 	/**
 	 * @runInSeparateProcess
 	 */
 	class SessionTest extends TestCase {
-		public function testHas(): void {
-			$config = Config::getInstance();
-			$request = Request::getInstance();
-			$session = new Session($config, $request);
-
-			$session -> set('user', ['id' => 1, 'name' => 'John Doe']);
-
-			$this -> assertTrue($session -> has('user'));
-			$this -> assertFalse($session -> has('nonexistent_key'));
-		}
-
-		public function testRemove(): void {
-			$config = Config::getInstance();
-			$request = Request::getInstance();
-			$session = new Session($config, $request);
-
-			$session -> set('user', ['id' => 1, 'name' => 'John Doe']);
-			$session -> remove('user');
-
-			$this -> assertFalse($session -> has('user'));
-		}
-
-		public function testDestroy(): void {
-			$config = Config::getInstance();
-			$request = Request::getInstance();
-			$session = new Session($config, $request);
-
-			$session -> set('user', ['id' => 1, 'name' => 'John Doe']);
-			$session -> destroy();
-
-			$this -> assertFalse($session -> has('user'));
-		}
-
-		public function testRegenerate(): void {
-			$config = Config::getInstance();
-			$request = Request::getInstance();
-			$session = new Session($config, $request);
-
-			$session_id_before = session_id();
-			$session -> regenerate();
-			$session_id_after = session_id();
-
-			$this -> assertNotEquals($session_id_before, $session_id_after);
-		}
-
-		public function testFlash(): void {
-			$config = Config::getInstance();
-			$request = Request::getInstance();
-			$session = new Session($config, $request);
-
-			$session -> flash('success', 'Data saved successfully.');
-
-			$flash_message = $session -> getFlash('success');
-
-			$this -> assertEquals('Data saved successfully.', $flash_message);
-			$this -> assertNull($session -> getFlash('success'));
-		}
+		// ADD TESTS FOR SESSION HERE.
 	}
