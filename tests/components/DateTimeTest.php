46964e851cc2f46520fa8b5628b227f51efafa09
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2024-01-04 19:47:19 +0100

    Update DateTimeTest.php
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

--- tests/components/DateTimeTest.php
+++ tests/components/DateTimeTest.php
@@ -4,7 +4,6 @@
 	use PHPUnit\Framework\TestCase;
 
 	use ClipStack\Component\DateTimeUtility;
-	use ClipStack\Component\Backbone\Config;
 
 	class DateTimeTest extends TestCase {
 		// ADD TEST FOR DATETIMEUTILITY HERE.
