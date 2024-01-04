448335d280a1d034f186d5df076031cee3bb4e99
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2024-01-04 19:53:34 +0100

    Update RequestTest.php
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

--- tests/components/RequestTest.php
+++ tests/components/RequestTest.php
@@ -9,66 +9,6 @@
 	 * @runInSeparateProcess
 	 */
 	class RequestTest extends TestCase {
-		public function testGetFullUrl(): void {
-			$server = [
-				'HTTPS' => 'on',
-				'HTTP_HOST' => 'example.com',
-				'REQUEST_URI' => '/path/to/page',
-			];
-
-			$request = Request::getInstance($server);
-			$full_url = $request -> getUrl();
-
-			$expected_url = 'https://example.com/path/to/page';
-			$this -> assertEquals($expected_url, $full_url);
-		}
-
-		public function testGetFullUrlWithoutHttps(): void {
-			$server = [
-				'HTTP_HOST' => 'example.com',
-				'REQUEST_URI' => '/path/to/page',
-			];
-
-			$request = Request::getInstance($server);
-			$full_url = $request -> getUrl();
-
-			$expected_url = 'http://example.com/path/to/page';
-			$this -> assertEquals($expected_url, $full_url);
-		}
-
-		public function testGetUri(): void {
-			$server = [
-				'REQUEST_URI' => '/path/to/page',
-			];
-
-			$request = Request::getInstance($server);
-			$uri = $request -> getUri();
-
-			$expected_uri = '/path/to/page';
-			$this -> assertEquals($expected_uri, $uri);
-		}
-
-		public function testGetHttpHost(): void {
-			$server = [
-				'HTTP_HOST' => 'example.com',
-			];
-
-			$request = Request::getInstance($server);
-			$http_host = $request -> getHttpHost();
-
-			$expected_http_host = 'example.com';
-			$this -> assertEquals($expected_http_host, $http_host);
-		}
-
-		public function testIsHttps(): void {
-			$server_with_https = ['HTTPS' => 'on'];
-			$server_without_https = [];
-
-			$request_with_https = Request::getInstance($server_with_https);
-			$request_without_https = Request::getInstance($server_without_https);
-
-			$this -> assertTrue($request_with_https -> isHttps());
-			$this -> assertFalse($request_without_https -> isHttps());
-		}
+		// ADD TESTS FOR REQUEST HERE.
 	}
 
